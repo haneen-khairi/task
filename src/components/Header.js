29716187b8774e9e4f6a8523fb3cb0d3a8ec6d24
zeyadalbazlamit <zeyad@zenhr.com>
2022-02-18 10:54:11 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Header = () => {
+  return <header className="App-header">header</header>;
+};
+export default Header;
---
+++ src/components/Slider.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+export default function SimpleSlider() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  const images = [
+    "https://images.unsplash.com/photo-1645079603178-8baea305c644?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNTl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1638913971251-832d29947de6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyMTh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1640622658799-54e6039d189b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyNzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+
+  ];
+  return (
+    <Slider {...settings} className="sliderContainer">
+      {images.map((item, index) => (
+          <img src={item} key={index} className="sliderImg"    />
+      ))}
+    </Slider>
+  );
+}
